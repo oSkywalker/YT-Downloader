86c8823ba422589878f6e52601a0ba9aae14f93d
oSkywalker <158072515+oSkywalker@users.noreply.github.com>
2024-02-23 17:16:13 +0000

    Started on Resolution Option

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from pathlib import Path
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
 
+
+
 # App Frame
 app = customtkinter.CTk() # Initialises app
 app.geometry("480x480") # Resolution of the app
 app.title("YT Downloader")
-        
+
+
+
 # Download -----Video-----
 def VideoDownload():
     try:
@@ -26,6 +30,8 @@ def VideoDownload():
 
     except:
         print("Invalid Link") #REMINDER# output this in the app instead of CLI
+
+
 
 # Download -----Audio-----
 def AudioDownload():
@@ -42,6 +48,8 @@ def AudioDownload():
     except:
             print("Invalid Link")
 
+
+
 # Progress Percentage
 def on_progress(stream, chunk, bytes_remaining):
     TotalSize = stream.filesize
@@ -54,14 +62,35 @@ def on_progress(stream, chunk, bytes_remaining):
     # Update Progress Bar
     ProgressBar.set(float(CompletionRate) / 100)
 
+
+
 # UI Elements
 title = customtkinter.CTkLabel(app, text="Enter URL", font=("arial", 22, "bold"))
 title.pack(padx=10, pady=50) # Create a text title
+
+
 
 # URL Input
 url = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url) 
 link.pack() # Create link input bar
+
+
+
+# Resolution DropDown
+def combo_callback(choice):
+     print(choice)   ## The choice needs to be changed to one of the YTobject.streams.<resolution type>
+    
+comboBox = customtkinter.CTkComboBox(master=app, values=["Video 1080p",
+                                                         "Video 720p",
+                                                         "Video 360p",
+                                                         "Audio 160kbps",
+                                                         "Audio 70kbps"],
+                                                         command=combo_callback) #The options need to refer back to the Download 
+comboBox.pack(padx=20, pady=20)
+comboBox.set("Resolution")
+
+
 
 # Download Progress
 ProgressPercent = customtkinter.CTkLabel(app, text="0%", font=("arial", 16, "bold"))
@@ -71,14 +100,22 @@ ProgressBar = customtkinter.CTkProgressBar(app, width=350, progress_color="red")
 ProgressBar.set(0)
 ProgressBar.pack()
 
+
+
+##### There should be --ONE-- Download Button because the option is selected on line 81-84
+
 DownloadPath = str(os.path.join(Path.home() / 'Downloads')) # Sets download path to /Downloads
 # Video Download Button
 download = customtkinter.CTkButton(app, font=("arial", 16, "bold"), fg_color="dark red", hover_color="red", text="MP4 Download", command=VideoDownload)
 download.pack(padx=10, pady=50) # Creates the button
 
+
+
 # Audio Download Button
 download = customtkinter.CTkButton(app, font=("arial", 16, "bold"), fg_color="dark red", hover_color="red", text="MP3 Download", command=AudioDownload)
 download.pack(padx=10, pady=5) # Creates the button
+
+
 
 # App Runner
 app.mainloop()
@@ -86,3 +123,9 @@ app.mainloop()
 ##### For v2 LIST AUDIO FILES AS MP4 FIX THIS
 
 ##### For v2 Change That Messages are inside of app not inside of TERMINAL
+
+##### For v2 Change Resolution to Specific
+
+##### For v2 Pin Thumbnail in app before downloading
+
+##### For v2 List Video name when inputting link
